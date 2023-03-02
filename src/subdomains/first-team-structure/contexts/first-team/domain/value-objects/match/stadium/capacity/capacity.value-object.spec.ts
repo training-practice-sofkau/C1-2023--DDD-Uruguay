bb6cebc4e2f5de4cd2dc6fa9344058fc39d3381c
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-03-02 13:16:57 -0300

    Adding match OV
    Adding some individual OV and one shared OV

---
+++ src/subdomains/first-team-structure/contexts/first-team/domain/value-objects/match/stadium/capacity/capacity.value-object.spec.ts
@@ -0,0 +1,7 @@
+import { CapacityValueObject } from './capacity.value-object';
+
+describe('CapacityValueObject', () => {
+  it('should be defined', () => {
+    expect(new CapacityValueObject()).toBeDefined();
+  });
+});
