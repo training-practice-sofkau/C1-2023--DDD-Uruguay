eacf752edf10ba52976e659c2ea494ff436c1947
brunof <dlqq@outlook.es>
2023-03-02 20:36:13 -0300

    Commit Create Object Value

---
+++ src/subdomains/-venta_web-/contexts/-Web-Context-/domain/value-objects/common-value-objects/uuid/uuid.value-object.spec.ts
@@ -0,0 +1,7 @@
+import { UuidValueObject } from './uuid.value-object';
+
+describe('UuidValueObject', () => {
+  it('should be defined', () => {
+    expect(new UuidValueObject()).toBeDefined();
+  });
+});
---
+++ src/subdomains/-venta_web-/contexts/-Web-Context-/domain/value-objects/cupon/porcentaje.value-object.spec.ts
@@ -0,0 +1,7 @@
+import { PorcentajeValueObject } from './porcentaje.value-object';
+
+describe('PorcentajeValueObject', () => {
+  it('should be defined', () => {
+    expect(new PorcentajeValueObject()).toBeDefined();
+  });
+});
