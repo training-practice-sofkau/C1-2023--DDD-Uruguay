6d10606f0727ec2cb05ff871ad4a18cfdcf50097
Cristian Tironi <tironi741@gmail.com>
2023-03-02 17:13:34 -0300

    Value objects Consumption
    Creamos los value objects de Consumption

---
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/extra/extra.value-object.spec.ts
@@ -0,0 +1,7 @@
+import { ExtraValueObject } from './extra.value-object';
+
+describe('ExtraValueObject', () => {
+  it('should be defined', () => {
+    expect(new ExtraValueObject()).toBeDefined();
+  });
+});
