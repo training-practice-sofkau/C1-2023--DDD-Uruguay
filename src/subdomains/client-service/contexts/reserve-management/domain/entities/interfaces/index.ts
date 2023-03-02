ae589fc772857d342718c10bfcf5768b38a02bc4
Cristian Tironi <tironi741@gmail.com>
2023-03-02 13:34:24 -0300

    CheckOut entities interfaces
    Creamos las interfaces de la entidad checkOut: consumption y invoice

--- src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/index.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/index.ts
@@ -1,1 +1,6 @@
-
+export * from './consumption.domain-entity.interface';
+export * from './customer.domain-entity.interface';
+export * from './guest.domain-entity.interface';
+export * from './invoice.domain-entity.interface';
+export * from './room-key.domain-entity.interface';
+export * from './room.domain-entity.interface';
