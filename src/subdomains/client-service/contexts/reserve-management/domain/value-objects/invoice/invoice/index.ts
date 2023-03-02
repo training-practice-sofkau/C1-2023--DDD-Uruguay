39247e3c75c705e2f6b600ae36a44917893f145c
Cristian Tironi <tironi741@gmail.com>
2023-03-02 15:57:00 -0300

    Value objects Invoice
    Creamos los value objects de Invoice

---
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/invoice/invoice/index.ts
@@ -0,0 +1,2 @@
+
+export * from './invoice.value-object';
---
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/invoice/invoice/invoice.value-object.spec.ts
@@ -0,0 +1,7 @@
+import { InvoiceValueObject } from './invoice.value-object';
+
+describe('InvoiceValueObject', () => {
+  it('should be defined', () => {
+    expect(new InvoiceValueObject()).toBeDefined();
+  });
+});
