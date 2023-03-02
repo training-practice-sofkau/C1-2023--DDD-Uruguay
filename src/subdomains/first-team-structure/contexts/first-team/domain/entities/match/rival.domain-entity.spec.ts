a224632405514584b5cf98ef1d931b3a04145dff
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-03-02 10:54:40 -0300

    Match entities
    All entities of Match AR

---
+++ src/subdomains/first-team-structure/contexts/first-team/domain/entities/match/rival.domain-entity.spec.ts
@@ -0,0 +1,7 @@
+import { RivalDomainEntity } from './rival.domain-entity';
+
+describe('RivalDomainEntity', () => {
+  it('should be defined', () => {
+    expect(new RivalDomainEntity()).toBeDefined();
+  });
+});
---
+++ src/subdomains/first-team-structure/contexts/first-team/domain/entities/match/stadium.domain-entity.spec.ts
@@ -0,0 +1,7 @@
+import { StadiumDomainEntity } from './stadium.domain-entity';
+
+describe('StadiumDomainEntity', () => {
+  it('should be defined', () => {
+    expect(new StadiumDomainEntity()).toBeDefined();
+  });
+});
