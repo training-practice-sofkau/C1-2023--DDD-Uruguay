b4ee4fcf19fd22015a06cb4f32787d2914b6d48d
Cristian Tironi <tironi741@gmail.com>
2023-03-02 15:05:26 -0300

    Value objects Reserve
    Creamos los value objects de reserve

---
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/reserve/number-of-guests/number-of-guests.value-object.spec.ts
@@ -0,0 +1,7 @@
+import { NumberOfGuestsValueObject } from './number-of-guests.value-object';
+
+describe('NumberOfGuestsValueObject', () => {
+  it('should be defined', () => {
+    expect(new NumberOfGuestsValueObject()).toBeDefined();
+  });
+});
