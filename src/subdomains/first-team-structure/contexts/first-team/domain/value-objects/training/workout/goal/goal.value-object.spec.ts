dc4d8fbd96ca66d06a492029bd58e802e36918d5
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-03-02 13:18:49 -0300

    Training OVs
    two OV indiviuals and one shared

---
+++ src/subdomains/first-team-structure/contexts/first-team/domain/value-objects/training/workout/goal/goal.value-object.spec.ts
@@ -0,0 +1,7 @@
+import { GoalValueObject } from './goal.value-object';
+
+describe('GoalValueObject', () => {
+  it('should be defined', () => {
+    expect(new GoalValueObject()).toBeDefined();
+  });
+});
