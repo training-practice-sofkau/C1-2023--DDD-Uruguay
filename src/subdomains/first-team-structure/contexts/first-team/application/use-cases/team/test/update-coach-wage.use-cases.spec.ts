d0b16314b5bdbceffd3aa041b1a6e63fd4531807
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-03-02 11:07:47 -0300

    Test team files
    Specs files of team AR

---
+++ src/subdomains/first-team-structure/contexts/first-team/application/use-cases/team/test/update-coach-wage.use-cases.spec.ts
@@ -0,0 +1,7 @@
+import { UpdateCoachWageUseCases } from './update-coach-wage.use-cases';
+
+describe('UpdateCoachWageUseCases', () => {
+  it('should be defined', () => {
+    expect(new UpdateCoachWageUseCases()).toBeDefined();
+  });
+});
---
+++ src/subdomains/first-team-structure/contexts/first-team/application/use-cases/team/test/update-player-wage.use-cases.spec.ts
@@ -0,0 +1,7 @@
+import { UpdatePlayerWageUseCases } from './update-player-wage.use-cases';
+
+describe('UpdatePlayerWageUseCases', () => {
+  it('should be defined', () => {
+    expect(new UpdatePlayerWageUseCases()).toBeDefined();
+  });
+});
