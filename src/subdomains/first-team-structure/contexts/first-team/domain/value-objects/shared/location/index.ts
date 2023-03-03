817089be31cb6f2beccb0978efc8396e90647f12
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-03-03 13:32:52 -0300

    Controllers
    Adding controllers in infrastrcture

--- src/subdomains/first-team-structure/contexts/first-team/domain/value-objects/shared/location/index.ts
+++
@@ -1,2 +0,0 @@
-
-export * from './location.value-object';
--- src/subdomains/first-team-structure/contexts/first-team/domain/value-objects/shared/location/location.value-object.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LocationValueObject } from './location.value-object';
-
-describe('LocationValueObject', () => {
-  it('should be defined', () => {
-    expect(new LocationValueObject()).toBeDefined();
-  });
-});
