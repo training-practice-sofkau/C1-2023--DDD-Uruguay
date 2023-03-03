1a892d5c8209ce747291e4d4b692b364bb5f27db
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-03-03 10:28:13 -0300

    subDomain RRHH se implemento value object nombre

--- src/subdomains/rrhh/contexts/__CONTEXTO__1__/domain/value-objects/empleado/nombre/nombre.value-object.ts
+++ src/subdomains/rrhh/contexts/__CONTEXTO__1__/domain/value-objects/empleado/nombre/nombre.value-object.ts
@@ -1,2 +1,27 @@
-import {  } from "../../../../../../../../libs";
-export class NombreValueObject {}
+import { IErrorValueObject } from 'src/libs';
+import { ValueObjectBase } from '../../../../../../../../libs/sofka/bases/object-value.base';
+
+export class Nombre implements ValueObjectBase<string>{
+    
+    get value(): string {
+        throw new Error('Method not implemented.');
+    }
+    set value(value: string) {
+        throw new Error('Method not implemented.');
+    }
+    validateData(): void {
+        throw new Error('Method not implemented.');
+    }
+    hasErrors(): boolean {
+        throw new Error('Method not implemented.');
+    }
+    getErrors(): IErrorValueObject[] {
+        throw new Error('Method not implemented.');
+    }
+    protected setError(error: IErrorValueObject): void {
+        throw new Error('Method not implemented.');
+    }
+    valueOf(): string {
+        throw new Error('Method not implemented.');
+    }
+}
