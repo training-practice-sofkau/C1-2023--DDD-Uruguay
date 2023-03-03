37a929d6f8ecd447ee5453285a2a5112903ba016
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-03-03 15:51:11 -0300

    caso de uso .retribucion y agrege el ddd

---
+++ src/subdomains/rrhh/contexts/__CONTEXTO__1__/application/use-cases/retribucion/modificar-pago.use-case.spec.ts
@@ -0,0 +1,7 @@
+import { ModificarPagoUseCase } from './modificar-pago.use-case';
+
+describe('ModificarPagoUseCase', () => {
+  it('should be defined', () => {
+    expect(new ModificarPagoUseCase()).toBeDefined();
+  });
+});
---
+++ src/subdomains/rrhh/contexts/__CONTEXTO__1__/application/use-cases/tramite/agregar-negociacion.use-case.spec.ts
@@ -0,0 +1,7 @@
+import { AgregarNegociacionUseCase } from './agregar-negociacion.use-case';
+
+describe('AgregarNegociacionUseCase', () => {
+  it('should be defined', () => {
+    expect(new AgregarNegociacionUseCase()).toBeDefined();
+  });
+});
