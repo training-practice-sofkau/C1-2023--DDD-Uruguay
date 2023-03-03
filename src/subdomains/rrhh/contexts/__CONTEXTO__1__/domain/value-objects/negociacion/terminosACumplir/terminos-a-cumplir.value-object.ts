5ea2ced7ff462a961d05a9947ad9b52577079770
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2023-03-03 10:33:06 -0300

    subDomain RRHH se implemento value object terminosACumplir

--- src/subdomains/rrhh/contexts/__CONTEXTO__1__/domain/value-objects/negociacion/terminosACumplir/terminos-a-cumplir.value-object.ts
+++ src/subdomains/rrhh/contexts/__CONTEXTO__1__/domain/value-objects/negociacion/terminosACumplir/terminos-a-cumplir.value-object.ts
@@ -1,1 +1,25 @@
-export class TerminosACumplirValueObject {}
+import { IErrorValueObject, ValueObjectBase } from 'src/libs';
+export class TerminosACumplir implements ValueObjectBase<string> {
+    
+    get value(): string {
+        throw new Error('Method not implemented.');
+    }
+    set value(value: string) {
+        throw new Error('Method not implemented.');
+    }
+    validateData(): void {
+        throw new Error('Method not implemented.');
+    }
+    hasErrors(): boolean {
+        throw new Error('Method not implemented.');
+    }
+    getErrors(): IErrorValueObject[] {
+        throw new Error('Method not implemented.');
+    }
+    protected setError(error: IErrorValueObject): void {
+        throw new Error('Method not implemented.');
+    }
+    valueOf(): string {
+        throw new Error('Method not implemented.');
+    }
+}
