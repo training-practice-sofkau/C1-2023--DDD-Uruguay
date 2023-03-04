00b0b9eb8a0351667d7071b15b81f460bb54d091
Cristian Tironi <tironi741@gmail.com>
2023-03-04 18:42:39 -0300

    Objetos de valor y Validaciones
    Creamos una libreria para validaciones e implementamos nuestro primer Objeto de Valor ID

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/commons/id/id.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/commons/id/id.value-object.ts
@@ -1,1 +1,65 @@
-export class IdValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/';
+import { v4 as uuid } from "uuid";
+import { IsUUID } from 'src/libs/validations/';
+
+export class IdValueObject extends ValueObjectBase<string>{
+
+    constructor(value?: string){
+        super(value ? value : uuid());
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+        this.validateLength();
+        this.validateStructure();
+    }
+
+    /**
+     *Validamos que la estructura es un UUID v4
+     *
+     * @private
+     * @memberof IdValueObject
+     */
+    private validateStructure(): void{
+        if(this.value && IsUUID(this.value) === false) {
+            const error = {
+                field: 'ID',
+                message: `El ID: ${this.value} , no es una estructura UUID v4 valida`
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos que el objeto no sea null o este vacio
+     *
+     * @private
+     * @memberof IdValueObject
+     */
+    private validateEmpty(): void {
+        if(this.value === null || this.value === undefined) {
+            const error = {
+                field: 'ID',
+                message: 'No se proporciono un ID'
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos la longitud del objeto
+     *
+     * @private
+     * @memberof IdValueObject
+     */
+    private validateLength(): void {
+        if(this.value.length > 36) {
+            const error = {
+                field: 'ID',
+                message: 'Se proporciono un ID muy largo'
+            };
+            this.setError(error);
+        }
+    }
+    
+}
