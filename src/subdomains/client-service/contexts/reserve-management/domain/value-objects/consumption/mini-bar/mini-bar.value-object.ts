fd678693e17a6f98cf7a466cdc27f9ecb86243ed
Cristian Tironi <tironi741@gmail.com>
2023-03-05 16:59:23 -0300

    Objeto de valor MiniBar
    Implementamos el objeto de valor mini-bar y sus validaciones

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/mini-bar/mini-bar.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/mini-bar/mini-bar.value-object.ts
@@ -1,1 +1,45 @@
-export class MiniBarValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/';
+
+export class MiniBarValueObject extends ValueObjectBase<number>{
+    
+    constructor(value: number) {
+        super(value);
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+        this.validatePositive();
+    }
+
+    /**
+     *Validamos que el objeto no sea null o este vacio
+     *
+     * @private
+     * @memberof MiniBarValueObject
+     */
+    private validateEmpty(): void {
+        if (this.value === null || this.value === undefined) {
+            const error = {
+                field: 'MiniBar',
+                message: 'No se proporciono un valor'
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos que el valor no sea negativo
+     *
+     * @private
+     * @memberof MiniBarValueObject
+     */
+    private validatePositive(): void {
+        if (this.value < 0) {
+            const error = {
+                field: 'MiniBar',
+                message: 'No se puede ingresar un costo negativo'
+            };
+            this.setError(error);
+        }
+    }
+}
