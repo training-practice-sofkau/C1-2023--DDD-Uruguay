ed96607929364c646f56ec0e99e8d37f3c48c484
Cristian Tironi <tironi741@gmail.com>
2023-03-05 16:57:13 -0300

    Objeto de valor Laundry
    Implementamos el objeto de valor laundry y sus validaciones

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/laundry/laundry.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/laundry/laundry.value-object.ts
@@ -1,1 +1,45 @@
-export class LaundryValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/';
+
+export class LaundryValueObject extends ValueObjectBase<number>{
+    
+    constructor(value: number) {
+        super(value);
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+        this.validatePositive();
+    }
+
+    /**
+     *Validamos que el objeto no sea null o este vacio
+     *
+     * @private
+     * @memberof LaundryValueObject
+     */
+    private validateEmpty(): void {
+        if (this.value === null || this.value === undefined) {
+            const error = {
+                field: 'Laundry',
+                message: 'No se proporciono un valor'
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos que el valor no sea negativo
+     *
+     * @private
+     * @memberof LaundryValueObject
+     */
+    private validatePositive(): void {
+        if (this.value < 0) {
+            const error = {
+                field: 'Laundry',
+                message: 'No se puede ingresar un costo negativo'
+            };
+            this.setError(error);
+        }
+    }
+}
