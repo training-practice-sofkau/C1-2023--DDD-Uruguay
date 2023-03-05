de7f87555921a87520f921df7306dc69fe981f79
Cristian Tironi <tironi741@gmail.com>
2023-03-05 16:52:18 -0300

    Objeto de valor ConsumptionFood
    Implementamos el objeto de valor consumption-food y sus validaciones

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/consumption-food/consumption-food.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/consumption-food/consumption-food.value-object.ts
@@ -1,1 +1,45 @@
-export class ConsumptionFoodValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/';
+
+export class ConsumptionFoodValueObject extends ValueObjectBase<number>{
+
+    constructor(value: number) {
+        super(value);
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+        this.validatePositive();
+    }
+
+    /**
+     *Validamos que el objeto no sea null o este vacio
+     *
+     * @private
+     * @memberof ConsumptionFoodValueObject
+     */
+    private validateEmpty(): void {
+        if (this.value === null || this.value === undefined) {
+            const error = {
+                field: 'ConsumptionFood',
+                message: 'No se proporciono un valor'
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos que el valor no sea negativo
+     *
+     * @private
+     * @memberof ConsumptionFoodValueObject
+     */
+    private validatePositive(): void {
+        if (this.value < 0) {
+            const error = {
+                field: 'ConsumptionFood',
+                message: 'No se puede ingresar un costo negativo'
+            };
+            this.setError(error);
+        }
+    }
+}
