52dc601036ea9a26f51c2ed383c58b11423aebc3
Cristian Tironi <tironi741@gmail.com>
2023-03-05 16:54:29 -0300

    Objeto de valor Extra
    Implementamos el objeto de valor extra y sus validaciones

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/extra/extra.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/consumption/extra/extra.value-object.ts
@@ -1,1 +1,46 @@
-export class ExtraValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/';
+
+export class ExtraValueObject extends ValueObjectBase<number>{
+    
+    constructor(value: number) {
+        super(value);
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+        this.validatePositive();
+    }
+
+    
+    /**
+     *Validamos que el objeto no sea null o este vacio
+     *
+     * @private
+     * @memberof ExtraValueObject
+     */
+    private validateEmpty(): void {
+        if (this.value === null || this.value === undefined) {
+            const error = {
+                field: 'Extra',
+                message: 'No se proporciono un valor'
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos que el valor no sea negativo
+     *
+     * @private
+     * @memberof ExtraValueObject
+     */
+    private validatePositive(): void {
+        if (this.value < 0) {
+            const error = {
+                field: 'Extra',
+                message: 'No se puede ingresar un valor negativo'
+            };
+            this.setError(error);
+        }
+    }
+}
