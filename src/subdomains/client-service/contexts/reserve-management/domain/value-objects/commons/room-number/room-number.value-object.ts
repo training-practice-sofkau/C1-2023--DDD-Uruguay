30c43febd40be2098b467c06177cfd32a5d8ff83
Cristian Tironi <tironi741@gmail.com>
2023-03-05 13:31:18 -0300

    Objeto de valor RoomNumber
    Implementamos el objeto de valor room-number y sus validaciones

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/commons/room-number/room-number.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/commons/room-number/room-number.value-object.ts
@@ -1,1 +1,53 @@
-export class RoomNumberValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/';
+
+export class RoomNumberValueObject extends ValueObjectBase<number>{
+
+    constructor(value: number) {
+        super(value);
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+        this.validateExist();
+    }
+
+    /**
+     *Validamos que el objeto no sea null o este vacio
+     *
+     * @private
+     * @memberof RoomNumberValueObject
+     */
+    private validateEmpty(): void {
+        if (this.value === null || this.value === undefined) {
+            const error = {
+                field: 'RoomNumber',
+                message: 'No se proporciono un numero de habitacion'
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos que el valor dado corresponda a una habitacion valida
+     * entre 1 y 10
+     *
+     * @private
+     * @memberof RoomNumberValueObject
+     */
+    private validateExist(): void {
+        if (this.value < 1) {
+            const error = {
+                field: 'RoomNumber',
+                message: 'Se proporciono un numero bajo'
+            };
+            this.setError(error);
+        }
+        if (this.value > 10) {
+            const error = {
+                field: 'RoomNumber',
+                message: 'Se proporciono un numero muy alto'
+            };
+            this.setError(error);
+        }
+    }
+}
