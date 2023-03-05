81e0d17d81f2121db545b1fa8be82958c8b46520
Cristian Tironi <tironi741@gmail.com>
2023-03-05 12:03:29 -0300

    Objeto de valor NumberOfGuests
    Implementamos el objeto de valor number-of-guests y sus validaciones

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/reserve/number-of-guests/number-of-guests.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/reserve/number-of-guests/number-of-guests.value-object.ts
@@ -1,1 +1,45 @@
-export class NumberOfGuestsValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/';
+
+export class NumberOfGuestsValueObject extends ValueObjectBase<number>{
+
+    constructor(value?: number) {
+        super(value);
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+        this.validateMaxCapacity();
+    }
+
+    /**
+     *Validamos que el objeto no sea null, este vacio o sea un valor menor a 1
+     *
+     * @private
+     * @memberof NumberOfGuestsValueObject
+     */
+    private validateEmpty(): void {
+        if (this.value === null || this.value === undefined || this.value < 1) {
+            const error = {
+                field: 'NumberOfGuests',
+                message: 'No se proporciono ningun huesped'
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos que el numero de huespedes no sobrepase la capacidad del hotel
+     *
+     * @private
+     * @memberof NumberOfGuestsValueObject
+     */
+    private validateMaxCapacity(): void {
+        if (this.value > 50) {
+            const error = {
+                field: 'NumberOfGuests',
+                message: 'El hotel no cuenta con tantas habitaciones disponibles'
+            };
+            this.setError(error);
+        }
+    }
+}
