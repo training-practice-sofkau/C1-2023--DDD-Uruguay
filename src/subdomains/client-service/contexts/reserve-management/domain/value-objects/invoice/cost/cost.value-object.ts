01205ba884c24d13b7880ec231e8c636e2567184
Cristian Tironi <tironi741@gmail.com>
2023-03-05 16:45:58 -0300

    Objeto de valor Cost
    Implementamos el objeto de valor cost y sus validaciones

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/invoice/cost/cost.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/invoice/cost/cost.value-object.ts
@@ -1,1 +1,45 @@
-export class CostValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/';
+
+export class CostValueObject extends ValueObjectBase<number>{
+
+    constructor(value: number) {
+        super(value);
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+        this.validatePositive();
+    }
+
+    /**
+     *Validamos que el objeto no sea null o este vacio
+     *
+     * @private
+     * @memberof CostValueObject
+     */
+    private validateEmpty(): void {
+        if (this.value === null || this.value === undefined) {
+            const error = {
+                field: 'Cost',
+                message: 'No se proporciono un valor valido al costo'
+            };
+            this.setError(error);
+        }
+    }
+
+    /**
+     *Validamos que el valor no sea negativo
+     *
+     * @private
+     * @memberof CostValueObject
+     */
+    private validatePositive(): void {
+        if (this.value < 0) {
+            const error = {
+                field: 'Cost',
+                message: 'No se puede ingresar un costo negativo'
+            };
+            this.setError(error);
+        }
+    }
+}
