1b800ee7051879369c50bd15fa3d21ecbe5bbfb3
Cristian Tironi <tironi741@gmail.com>
2023-03-04 21:33:26 -0300

    Objeto de valor Date
    Implementamos el objeto de valor Date y sus validaciones

--- src/subdomains/client-service/contexts/reserve-management/domain/value-objects/commons/date/date.value-object.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/value-objects/commons/date/date.value-object.ts
@@ -1,1 +1,28 @@
-export class DateValueObject {}
+import { ValueObjectBase } from 'src/libs/sofka/bases/';
+
+export class DateValueObject extends ValueObjectBase<Date> {
+
+    constructor(value?: Date) {
+        super(value);
+    }
+
+    validateData(): void {
+        this.validateEmpty();
+    }
+
+    /**
+     *Validamos que el objeto no sea null o este vacio
+     *
+     * @private
+     * @memberof DateValueObject
+     */
+    private validateEmpty(): void {
+        if (this.value === null || this.value === undefined) {
+            const error = {
+                field: 'Date',
+                message: 'No se proporciono una fecha'
+            };
+            this.setError(error);
+        }
+    }
+}
