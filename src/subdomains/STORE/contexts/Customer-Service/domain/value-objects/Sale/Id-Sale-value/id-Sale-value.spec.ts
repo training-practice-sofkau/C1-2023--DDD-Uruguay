fcf883c60964631193075d8fb1b4d9abd280cbbc
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:39:46 -0300

    agregando objeto de valor

---
+++ src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Sale/Id-Sale-value/id-Sale-value.spec.ts
@@ -0,0 +1,24 @@
+import { IdSaleValueObject } from './id-Sale-value';
+
+describe("validate Id", () => {
+
+    it("should add an error if the value is invalid", () => {
+      const value = "invalid-value";
+      const seller = new IdSaleValueObject(value);
+    
+      seller.validateData();
+    
+      expect(seller.hasErrors()).toBeFalsy();
+     
+    });
+    it("Aca funciona :D", () => {
+      const value = "f0b9c684-7d28-4c58-9947-f66f71dd3ab3";
+      const seller = new IdSaleValueObject(value);
+    
+      seller.validateData();
+    
+      expect(seller.hasErrors()).toBeTruthy();
+     
+    });
+    
+    })
