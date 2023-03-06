bde3641d6c8cedbc548f5dccc572c4b8961696c8
brunof <dlqq@outlook.es>
2023-03-06 18:35:31 -0300

    Commit Validations

---
+++ src/libs/sofka/interface/event-publisher.interface.ts
@@ -0,0 +1,6 @@
+export interface IEventPublisher {
+
+    send<Result = any, Input = any>(pattern: any, data: Input): Promise<Result>;
+    emit<Result = any, Input = any>(pattern: any, data: Input): Promise<Result>;
+    
+  }
--- src/subdomains/-venta_web-/contexts/-Web-Context-/domain/value-objects/common-value-objects/uuid/uuid.value-object.ts
+++ src/subdomains/-venta_web-/contexts/-Web-Context-/domain/value-objects/common-value-objects/uuid/uuid.value-object.ts
@@ -1,1 +1,35 @@
-export class UuidValueObject {}
+import { ValueObjectBase } from "src/libs";
+import { IsUUID } from "src/libs/sofka/validations/uuid-validation";
+import {v4 as uuid} from 'uuid';
+
+export class UuidValueObject extends ValueObjectBase<string>{
+    
+    //Con el constructor objengo el valor 
+    constructor(value? : string){
+        super(value ? value : uuid())
+    }
+
+    //Esta funcion es la que voy a lanzar en el caso de uso
+    validateData(): void {
+       this.validateUUID();
+    }
+
+
+    private validateUUID() : void {
+        
+        if(this.value && IsUUID(this.value)=== false){
+
+            const error = {field : 'Id',
+            message : 'El Id no es valido!'
+            }
+
+            this.setError(error) // seteo el error si existe
+        }
+
+        // Otro if si necesito hacer mas validaciones
+
+    }
+
+
+    
+}
