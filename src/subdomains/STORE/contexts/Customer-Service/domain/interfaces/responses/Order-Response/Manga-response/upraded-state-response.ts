46a450366b6b317378570a3cee956b0a47cf740c
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 12:13:14 -0300

    Agregando responses

--- src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/responses/Order-Response/Manga-response/upraded-state-response.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/responses/Order-Response/Manga-response/upraded-state-response.ts
@@ -1,1 +1,6 @@
-export class UpradedStateResponse {}
+import { MangaDomainBase } from "../../../../entities/Order-domain/manga-domain-entity";
+
+export class UpradedStateResponse {
+    succes: boolean;
+    data: MangaDomainBase | null 
+}
--- src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/responses/Order-Response/manga-obtained-response.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/responses/Order-Response/manga-obtained-response.ts
@@ -1,1 +1,5 @@
-export class MangaObtainedResponse {}
+import { MangaDomainBase } from '../../../entities/Order-domain/manga-domain-entity';
+export class MangaObtainedResponse {
+    succes: boolean;
+    data: MangaDomainBase | null 
+}
