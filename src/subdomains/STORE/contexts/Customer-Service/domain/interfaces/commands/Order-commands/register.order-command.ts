170ccde196a05810cbd983c5fd1ac0587bd35068
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:33:16 -0300

    cambios en interfaces

--- src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/commands/Order-commands/register.order-command.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/commands/Order-commands/register.order-command.ts
@@ -1,1 +1,7 @@
-export class RegisterOrder {}
+import { ClientDomainBase } from "../../../entities/Order-domain/client-domain-entity";
+import { MangaDomainBase } from "../../../entities/Order-domain/manga-domain-entity";
+
+export class RegisterOrder {    
+    client: ClientDomainBase;
+    Manga: MangaDomainBase;
+}
--- src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/responses/Order-Response/registerd-order-response.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/responses/Order-Response/registerd-order-response.ts
@@ -1,1 +1,5 @@
-export class RegisterdOrderResponse {}
+import { OrderDomainEntityBase } from '../../../entities/Order-domain/Order-domain-entity';
+export class RegisterdOrderResponse {
+    succes: boolean;
+    data: OrderDomainEntityBase | null 
+}
