11af1a6bd4eebd44d537dbd8ad2c83ca28ca5730
Cristian Tironi <tironi741@gmail.com>
2023-03-06 10:35:57 -0300

    Entity Interfaces de Room y Customer
    Implementamos las interfaces para las entidades customer y room

--- src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/room.domain-entity.interface.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/room.domain-entity.interface.ts
@@ -1,1 +1,20 @@
-export interface RoomDomainEntity {}
+import {
+    AccommodationValueObject,
+    IdValueObject,
+    LocationValueObject,
+    RoomNumberValueObject,
+    StateValueObject,
+    TypeValueObject
+} from "../../value-objects";
+
+export interface IRoomDomainEntity {
+    roomId: string | IdValueObject;
+    location: string | LocationValueObject;
+    accommodation: string | AccommodationValueObject;
+    type: string | TypeValueObject;
+    state: boolean | StateValueObject;
+    roomNumber: number | RoomNumberValueObject;
+    createdAt?: number | Date;
+    updatedAt?: number | Date;
+    deletedAt?: number | Date;
+}
