c98c45616e4eff3c1ecd678fad1816886817d3fd
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:36:15 -0300

    agregando objeto de valor

--- src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Order/Manga/idmanga-value/idmanga-value.spec.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Order/Manga/idmanga-value/idmanga-value.spec.ts
@@ -1,7 +1,25 @@
 import { IdmangaValue } from './idmanga-value';
 
-describe('IdmangaValue', () => {
-  it('should be defined', () => {
-    expect(new IdmangaValue()).toBeDefined();
+describe("validate Id", () => {
+
+  it("should add an error if the value is invalid", () => {
+    const value = "invalid-value";
+    const mangaId = new IdmangaValue(value);
+  
+    mangaId.validateData();
+  
+    expect(mangaId.hasErrors()).toBeFalsy();
+    
+   
   });
-});
+  it("Test UUID ", () => {
+    const value = "f0b9c684-7d28-4c58-9947-f66f71dd3ab3";
+    const seller = new IdmangaValue(value);
+  
+    seller.validateData();
+  
+    expect(seller.hasErrors()).toBeTruthy();
+   
+  });
+  
+  })
