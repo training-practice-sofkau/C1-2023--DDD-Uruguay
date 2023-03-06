dfae9c148c8701b40811afd9011bfdaac02037e0
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 11:17:44 -0300

    update manga comands

--- src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/commands/Order-commands/update-manga-stock-command.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/commands/Order-commands/update-manga-stock-command.ts
@@ -1,1 +1,5 @@
-export class UpdateMangaStock {}
+import { MangaDomainBase } from '../../../entities/Order-domain/manga-domain-entity';
+export class UpdateMangaStock {
+    ClientID: string
+    ClientData: Partial<MangaDomainBase>
+}
