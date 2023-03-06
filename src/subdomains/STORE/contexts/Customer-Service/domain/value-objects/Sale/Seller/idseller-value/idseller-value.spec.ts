a5959206106411487ddbc57e06b34ef8e552fd50
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:40:07 -0300

    agregando objeto de valor

--- src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Sale/Seller/idseller-value/idseller-value.spec.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Sale/Seller/idseller-value/idseller-value.spec.ts
@@ -1,7 +1,26 @@
+
 import { IdsellerValue } from './idseller-value';
 
-describe('IdsellerValue', () => {
-  it('should be defined', () => {
-    expect(new IdsellerValue()).toBeDefined();
-  });
+describe("validate Id", () => {
+
+it("should add an error if the value is invalid", () => {
+  const value = "invalid-value";
+  const seller = new IdsellerValue(value);
+
+  seller.validateData();
+
+  expect(seller.hasErrors()).toBeFalsy();
+  
+ 
 });
+it("Aca funciona :D", () => {
+  const value = "f0b9c684-7d28-4c58-9947-f66f71dd3ab3";
+  const seller = new IdsellerValue(value);
+
+  seller.validateData();
+
+  expect(seller.hasErrors()).toBeTruthy();
+ 
+});
+
+})
