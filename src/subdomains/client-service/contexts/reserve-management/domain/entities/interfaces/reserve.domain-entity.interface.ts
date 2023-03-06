0cf025a34f2cb81384771ba930a1b0bf6a3455a6
Cristian Tironi <tironi741@gmail.com>
2023-03-06 11:23:13 -0300

    Reserve interface
    Implementamos la interface para reserve y actualizamos algunos archivos barril

--- src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/reserve.domain-entity.interface.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/reserve.domain-entity.interface.ts
@@ -1,1 +1,14 @@
-export interface ReserveDomainEntity {}
+import { DateValueObject, IdValueObject, NumberOfGuestsValueObject } from "../../value-objects";
+import { ICustomerDomainEntity, IRoomDomainEntity } from "../interfaces";
+
+export interface ReserveDomainEntity {
+    reserveId: string | IdValueObject;
+    startDate: Date | DateValueObject;
+    endDate?: Date | DateValueObject;
+    numberOfGuests: number | NumberOfGuestsValueObject;
+    room: IRoomDomainEntity;
+    customer: ICustomerDomainEntity;
+    createdAt?: number | Date;
+    updatedAt?: number | Date;
+    deletedAt?: number | Date;
+}
