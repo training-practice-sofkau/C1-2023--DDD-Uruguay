fc7c34f096226722da9f9dcf13aecd5a9f5b2239
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:40:20 -0300

    agregando objeto de valor

--- src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Sale/Seller/name-value/name-value.spec.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Sale/Seller/name-value/name-value.spec.ts
@@ -1,7 +1,39 @@
-import { NameValue } from './name-value';
+import { NameSellerValue } from "../name-value";
 
-describe('NameValue', () => {
-  it('should be defined', () => {
-    expect(new NameValue()).toBeDefined();
+  describe("validate Name", () => {
+    it("test name" , () => {
+
+      const value = "Franco Torres";
+
+      const nameSellerValue = new NameSellerValue(value);
+
+      nameSellerValue.validateData();
+
+      expect(nameSellerValue.hasErrors()).toBeTruthy();
+
+    });
+
+    it("prueba name con numeros " , () => {
+
+        const value = "Fr4nc0 Torres";
+  
+        const nameSellerValue = new NameSellerValue(value);
+  
+        nameSellerValue.validateData();
+  
+        expect(nameSellerValue.hasErrors()).toBeFalsy()
+        
+      });
+
+      it("prueba con espacios " , () => {
+
+        const value = "F R A N C O ";
+  
+        const nameSellerValue = new NameSellerValue(value);
+  
+        nameSellerValue.validateData();
+  
+        expect(nameSellerValue.hasErrors()).toBeFalsy();
+      
+      });
   });
-});
