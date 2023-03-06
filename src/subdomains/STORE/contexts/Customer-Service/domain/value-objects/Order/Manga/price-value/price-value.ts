6b842b95c4ad48a6668410dfc0ae37aa8aae22d2
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:36:50 -0300

    agregando objeto de valor

--- src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Order/Manga/price-value/price-value.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Order/Manga/price-value/price-value.ts
@@ -1,1 +1,39 @@
-export class PriceValue {}
+import { ValueObjectBase } from "src/libs/sofka/bases/object-value.base";
+import { isNumber } from "src/libs/validations/is-number.validations";
+
+
+export class PriceValue extends ValueObjectBase<number>{
+/**
+ * The constructor function is a special function that is called when an object is created from a
+ * class.
+ * @param {string} [value] - The value of the input.
+ */
+   
+  constructor(value?: number){
+      super(value);
+  }
+
+/**
+ * If the structure of the data is valid, then validate the data.
+ */
+
+  validateData(): void {
+  this.validateStructure()
+  }
+
+/* Validating the structure of the data. */
+
+private validateStructure(): void {
+    
+    if(this.value && !isNumber(this.value) === false)
+{
+    const error = {
+      field: 'Number',
+      message: 'El dato ingresado en "Number" no contiene una estructura valida '
+    }
+    this.setError(error)
+  }
+  
+
+}
+}
