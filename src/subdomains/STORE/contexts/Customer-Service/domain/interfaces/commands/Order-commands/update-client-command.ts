0df4659b88f85a12e952cf1d93ade047928d6260
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 11:16:50 -0300

    Update client commands

--- src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/commands/Order-commands/update-client-command.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/interfaces/commands/Order-commands/update-client-command.ts
@@ -1,1 +1,7 @@
-export class UpdateClient {}
+import { ClientDomainBase } from "../../../entities/Order-domain/client-domain-entity"
+
+export class UpdateClient {
+    ClientID: string
+    ClientData: Partial<ClientDomainBase>
+
+}
