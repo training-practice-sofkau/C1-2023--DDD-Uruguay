55a9f3ac662c55800f87dbf5c1cacacac5abc882
Cristian Tironi <tironi741@gmail.com>
2023-03-06 11:16:01 -0300

    Agregates entities
    Creamos las entidades e interfaces de nuestros agregados, que nos olvidamos previamente

---
+++ src/subdomains/client-service/contexts/reserve-management/domain/entities/check-in/check-in.domain-entity.spec.ts
@@ -0,0 +1,7 @@
+import { CheckInDomainEntity } from './check-in.domain-entity';
+
+describe('CheckInDomainEntity', () => {
+  it('should be defined', () => {
+    expect(new CheckInDomainEntity()).toBeDefined();
+  });
+});
