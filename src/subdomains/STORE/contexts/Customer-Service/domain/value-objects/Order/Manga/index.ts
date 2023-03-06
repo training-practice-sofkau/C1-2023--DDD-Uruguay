cfb632a42ac1d07a13befc524d04915802f3484e
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:37:27 -0300

    agregando objeto de valor

---
+++ src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Order/Manga/index.ts
@@ -0,0 +1,5 @@
+export * from './idmanga-value';
+export * from './name-value';
+export * from './price-value';
+export * from './sate-value';
+export * from './stock-value';
--- src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Order/Manga/stock-value/stock-value.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/value-objects/Order/Manga/stock-value/stock-value.ts
@@ -1,1 +1,39 @@
-export class StockValue {}
+import { ValueObjectBase } from "src/libs/sofka/bases/object-value.base";
+import { isStock } from "src/libs/validations/is-stock.validations";
+
+
+export class StockValue extends ValueObjectBase<number>{
+/**
+ * The constructor function is a special function that is called when an object is created from a
+ * class.
+ * @param {string} [value] - The value of the input.
+ */
+   
+  constructor(value?: number){
+      super(value);
+  }
+
+/**
+ * If the structure of the data is valid, then validate the data.
+ */
+
+  validateData(): void {
+  this.validateStructure()
+  }
+
+/* Validating the structure of the data. */
+
+private validateStructure(): void {
+    
+    if(this.value && !isStock(this.value) === false)
+{
+    const error = {
+      field: 'Stock',
+      message: 'El dato ingresado en "Stock" no contiene una estructura valida '
+    }
+    this.setError(error)
+  }
+  
+
+}
+}
