13e5df5b84817976e3d5c305d5e6915b4c967cd8
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:32:28 -0300

    order publisher

--- src/subdomains/STORE/contexts/Customer-Service/domain/events/publishers/order/Order-add-event-publisher.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/events/publishers/order/Order-add-event-publisher.ts
@@ -1,2 +1,16 @@
-export class OrderAddEventPublisher {
+import { EventPublisherBase } from "src/libs";
+import { OrderDomainEntityBase } from "../../../entities/Order-domain/Order-domain-entity";
+/**
+ *
+ *
+ * @export
+ * @abstract
+ * @class OrderAddEventPublisher
+ * @extends {EventPublisherBase<OrderDomainEntityBase>}
+ */
+export abstract class OrderAddEventPublisher  extends EventPublisherBase<OrderDomainEntityBase>{
+ 
+    publish(): void {
+        console.log('Ta todo funcionando o  a lo mejor no y este console log nunca se va  a ver ')
+    }
 }
