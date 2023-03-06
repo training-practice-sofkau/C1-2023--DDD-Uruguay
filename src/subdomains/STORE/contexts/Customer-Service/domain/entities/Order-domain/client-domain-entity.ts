1b65fdbecdb03b7bac2c59dee982b9648e2d8f2a
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 10:56:36 -0300

    Agregando comain de client

--- src/subdomains/STORE/contexts/Customer-Service/domain/entities/Order-domain/client-domain-entity.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/entities/Order-domain/client-domain-entity.ts
@@ -1,1 +1,9 @@
-export class ClientDomainBase  {}
+import { ClientNameValue, PhoneValue } from "../../value-objects";
+import { IdclientValue } from "../../value-objects/Sale/Bill/idclient-value/idclient-value";
+import { IClientEntity } from "../interfaces/Order/client.interface";
+
+export class ClientDomainBase  implements IClientEntity {
+    ClientID: IdclientValue;
+    Name: ClientNameValue;
+    Phone: PhoneValue;
+}
