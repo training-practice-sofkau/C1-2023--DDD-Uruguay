9dc3662c488716f6a7cb4724598a83fb550ed275
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 21:14:38 -0300

    client publisher

--- src/subdomains/STORE/contexts/Customer-Service/domain/events/publishers/order/client/modified-Phone-event-publisher.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/events/publishers/order/client/modified-Phone-event-publisher.ts
@@ -1,7 +1,13 @@
 import { EventPublisherBase } from "src/libs";
 import { ClientDomainBase } from "../../../../entities/Order-domain/client-domain-entity";
 
-export class PhoneModifiedEventPublisher  extends  EventPublisherBase<ClientDomainBase> {
-    publish(): void {
-console.log("Telefono modificado")    }
-}  
+export class PhoneModifiedEventPublisher <
+Response = ClientDomainBase
+>   extends EventPublisherBase<Response> {
+    publish<Result = any>(): Promise<Result> {
+        return this.emit(
+            'order-publish-event',
+            JSON.stringify({ data: this.response })
+        )
+}
+}
