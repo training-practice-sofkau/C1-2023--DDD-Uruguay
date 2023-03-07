ceaa926449d893d9398494730988d4db17daa86f
brunof <dlqq@outlook.es>
2023-03-06 21:11:58 -0300

    Commit Validation Costo

---
+++ src/libs/validations/costo-validation.ts
@@ -0,0 +1,6 @@
+export const costoValorOk = (value: number ): boolean =>{
+
+    if(value > 0) return true;
+
+    return false;
+}
--- src/subdomains/-venta_web-/contexts/-Web-Context-/domain/value-objects/common-value-objects/costo/costo.value-object.ts
+++ src/subdomains/-venta_web-/contexts/-Web-Context-/domain/value-objects/common-value-objects/costo/costo.value-object.ts
@@ -1,1 +1,34 @@
-export class CostoValueObject {}
+import { ValueObjectBase } from "src/libs/sofka/bases";
+
+export class CostoValueObject extends ValueObjectBase<number>{
+    
+
+    constructor(value? : number){
+        super(value ? value : null)
+    }
+    
+    
+    validateData(): void {
+       
+    }
+
+
+    private valorPositivo() : void {
+
+        //Valido la existencia de un valor
+        if (this.value) {
+            const error = { field: 'Costo', message: 'El costo esta vacio!'};
+
+            this.setError(error);
+        }
+
+        //Valido que el valor sea positivo
+        if (this.value < 0) {
+            const error = { field: 'Costo', message: 'El costo no puede ser menor a cero!'};
+
+            this.setError(error);
+        }
+
+
+    }
+}
