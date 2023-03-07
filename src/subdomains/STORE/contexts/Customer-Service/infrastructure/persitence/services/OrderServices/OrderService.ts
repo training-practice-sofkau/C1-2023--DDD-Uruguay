87b06435057809dd83b3a6dea4a0daa6ac8263d9
Franco Torres <franco.torres1705@gmail.com>
2023-03-07 17:42:48 -0300

    cambios  en service

--- src/subdomains/STORE/contexts/Customer-Service/infrastructure/persitence/services/OrderServices/OrderService.ts
+++ src/subdomains/STORE/contexts/Customer-Service/infrastructure/persitence/services/OrderServices/OrderService.ts
@@ -1,16 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { ClientDomainBase } from '../../../../domain/entities/Order-domain/client-domain-entity';
-import { MangaDomainBase } from '../../../../domain/entities/Order-domain/manga-domain-entity';
-import { OrderDomainEntityBase } from '../../../../domain/entities/Order-domain/Order-domain-entity';
-import { IAddClient } from '../../../../domain/interfaces/commands/Order-commands/add-client-command';
-import { IDeleteOrder } from '../../../../domain/interfaces/commands/Order-commands/delete-command';
-import { IGetClient } from '../../../../domain/interfaces/commands/Order-commands/get-client-command';
-import { IRegisterOrder } from '../../../../domain/interfaces/commands/Order-commands/register.order-command';
-import { IUpdateClient } from '../../../../domain/interfaces/commands/Order-commands/update-client-command';
-import { IUpdateMangaStock } from '../../../../domain/interfaces/commands/Order-commands/update-manga-stock-command';
-import { IUpdateOrder } from '../../../../domain/interfaces/commands/Order-commands/update-order-command';
-import { IGetMangaData } from '../../../../domain/interfaces/commands/Sale-commands/Bill-Comands/get-manga-data-command';
-import { IorderDomainService } from '../../../../domain/services/Order-domain-service';
+import { Injectable } from "@nestjs/common";
+import { OrderDomainEntityBase, ClientDomainBase, MangaDomainBase } from "../../../../domain/entities";
+import { IRegisterOrder, IGetClient, IUpdateOrder, IDeleteOrder, IGetMangaData, IAddClient, IUpdateMangaStock, IUpdateClient } from "../../../../domain/interfaces";
+import { IorderDomainService } from "../../../../domain/services";
 
 @Injectable()
 export class OrderService implements IorderDomainService {
