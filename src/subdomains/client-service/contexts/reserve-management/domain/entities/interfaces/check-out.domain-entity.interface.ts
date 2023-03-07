2550cbd3e4ba4acbbeeb96dd2634b5b0e91e77d2
Cristian Tironi <tironi741@gmail.com>
2023-03-06 21:20:31 -0300

    Interfaces CheckIn y CheckOut
    Implementamos las interfaces para los agregados CheckIn y CheckOut y todas sus entidades

--- src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/check-out.domain-entity.interface.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/check-out.domain-entity.interface.ts
@@ -1,1 +1,14 @@
-export interface CheckOutDomainEntity {}
+import { IdValueObject, DateValueObject, FullNameValueObject } from "../../value-objects";
+import { IConsumptionDomainEntity, IInvoiceDomainEntity } from ".";
+
+
+export interface ICheckOutDomainEntity {
+    checkOutId: string | IdValueObject;
+    endDate: Date | DateValueObject;
+    recepsionistName: string | FullNameValueObject;
+    invoice: IInvoiceDomainEntity;
+    consumption: IConsumptionDomainEntity;
+    createdAt?: number | Date;
+    updatedAt?: number | Date;
+    deletedAt?: number | Date;
+}
--- src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/guest.domain-entity.interface.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/entities/interfaces/guest.domain-entity.interface.ts
@@ -1,1 +1,15 @@
-export interface GuestDomainEntity {}
+import { 
+    DocumentValueObject, 
+    EmailValueObject, 
+    FullNameValueObject, 
+    IdValueObject, 
+    PhoneValueObject 
+} from "../../value-objects";
+
+export interface IGuestDomainEntity {
+    guestId: string | IdValueObject;
+    fullName: string | FullNameValueObject;
+    document: number | DocumentValueObject;
+    phone: string | PhoneValueObject;
+    email: string | EmailValueObject;
+}
