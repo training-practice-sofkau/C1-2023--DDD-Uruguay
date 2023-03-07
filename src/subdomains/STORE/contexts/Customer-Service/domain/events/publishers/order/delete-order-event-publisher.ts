1a61eda5acb0653f09301f09a17122202a948df0
Franco Torres <franco.torres1705@gmail.com>
2023-03-06 21:12:25 -0300

    update publish delete

--- src/subdomains/STORE/contexts/Customer-Service/domain/events/publishers/order/delete-order-event-publisher.ts
+++ src/subdomains/STORE/contexts/Customer-Service/domain/events/publishers/order/delete-order-event-publisher.ts
@@ -1,7 +1,13 @@
 import { EventPublisherBase } from "src/libs";
 import { OrderDomainEntityBase } from "../../../entities/Order-domain/Order-domain-entity";
 
-export class DeleteOrderEventPublisher extends EventPublisherBase<OrderDomainEntityBase> {
-    publish(): void {
-console.log("Cliente borrado")    }
+export class DeleteOrderEventPublisher <
+Response = OrderDomainEntityBase
+>   extends EventPublisherBase<Response> {
+    publish<Result = any>(): Promise<Result> {
+        return this.emit(
+            'order-delete',
+            JSON.stringify({ data: this.response })
+        )
+}
 }
