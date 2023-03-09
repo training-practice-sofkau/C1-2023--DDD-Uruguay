54461472ff6b2f80defd0666beecf3e91f557324
Franco Torres <franco.torres1705@gmail.com>
2023-03-09 10:11:25 -0300

    agregando cambios en register order

--- src/subdomains/STORE/contexts/Customer-Service/application/use-cases/Order-Use-case/register-order-case-use/register-order-case-use.ts
+++ src/subdomains/STORE/contexts/Customer-Service/application/use-cases/Order-Use-case/register-order-case-use/register-order-case-use.ts
@@ -1,1 +1,101 @@
-export class RegisterOrderCaseUse {}
+import { IUseCase, ValueObjectErrorHandler } from "src/libs";
+import { OrderAgregate } from "../../../../domain/aggregates/order.agregate";
+import { OrderDomainEntityBase } from "../../../../domain/entities";
+import { OrderAddEventPublisher } from "../../../../domain/events";
+import { IRegisterOrder, RegisterdOrderResponse } from "../../../../domain/interfaces";
+import { IorderDomainService } from "../../../../domain/services";
+import { OrderService } from '../../../../infrastructure/persitence/services/OrderServices/OrderService';
+
+export class RegisterOrderCaseUse<
+    Command extends IRegisterOrder = IRegisterOrder,
+    Response extends RegisterdOrderResponse =  RegisterdOrderResponse
+
+>
+    extends ValueObjectErrorHandler
+    implements IUseCase<Command, Response>
+{
+
+    private readonly OrderAgregate: OrderAgregate;
+
+    constructor(
+        private readonly orderService: OrderService,
+        private readonly RegisterOrderEventPublisher: OrderAddEventPublisher,
+    ) {
+        super();
+        this.OrderAgregate = new OrderAgregate({
+            orderService,
+            RegisterOrderEventPublisher
+        })
+    }
+
+    async execute(command?: Command): Promise<Response> {
+        const data = await this.executeCommand(command);
+
+        return { success: data ? true : false, data } as unknown as Response
+    }
+
+    private async executeCommand(
+        command: Command
+    ): Promise<OrderDomainEntityBase | null> {
+        const ValueObject = this.createValueObject(command);
+        this.validateValueObject(ValueObject);
+        const entity = this.createEntityClientDomain(ValueObject);
+        return this.exectueOrderAggregateRoot(entity)
+    }
+
+    private createValueObject(
+        command: Command
+    ): IorderDomainService {
+
+        const fullName = new FullNameValueObject(command.fullName);
+        const phone = new PhoneObjectValue(command.phone);
+
+        return {
+            fullName,
+            phone
+        }
+    }
+
+    private validateValueObject(
+        valueObject: IClientDomainEntity
+    ): void {
+        const {
+            fullName,
+            phone
+        } = valueObject
+
+        if (fullName instanceof FullNameValueObject && fullName.hasErrors())
+            this.setErrors(fullName.getErrors());
+
+        if (phone instanceof PhoneObjectValue && phone.hasErrors())
+            this.setErrors(phone.getErrors());
+
+        if (this.hasErrors() === true)
+            throw new ValueObjectException(
+                'Hay algunos errores en el comando ejecutado por AddClientUseCase',
+                this.getErrors(),
+            );
+
+    }
+
+    private createEntityClientDomain(
+        valueObject: IClientDomainEntity
+    ): ClientDomainEntitybase {
+
+        const {
+            fullName,
+            phone
+        } = valueObject
+
+        return new ClientDomainEntitybase({
+            fullName: fullName.valueOf(),
+            phone: phone.valueOf()
+        })
+    }
+
+    private exectueOrderAggregateRoot(
+        entity: ClientDomainEntitybase,
+    ): Promise<ClientDomainEntitybase | null> {
+        return this.orderAggregateRoot.registerClient(entity)
+    }
+}
