62a946d72cac3036b787c50c777da0aca4a199af
Cristian Tironi <tironi741@gmail.com>
2023-03-12 18:28:11 -0300

    Create CheckIn
    Implementamos el caso de uso create-check-in del agregado checkIn

--- src/subdomains/client-service/contexts/reserve-management/application/use-cases/check-in/create-check-in/create-check-in.use-case.ts
+++ src/subdomains/client-service/contexts/reserve-management/application/use-cases/check-in/create-check-in/create-check-in.use-case.ts
@@ -1,1 +1,111 @@
-export class CreateCheckInUseCase {}
+import {
+    IUseCase,
+    ValueObjectErrorHandler,
+    ValueObjectException
+} from "src/libs/sofka";
+import {
+    CheckInAggregate,
+    CheckInCreatedEventPublisher,
+    CheckInDomainEntity,
+    DateValueObject,
+    FullNameValueObject,
+    GuestObtainedEventPublisher,
+    ICheckInCreatedResponse,
+    ICheckInDomainEntity,
+    ICheckInDomainService,
+    ICreateCheckIn,
+    RoomKeyObtainedEventPublisher
+} from "../../../../domain";
+import {
+    GetGuestUseCase,
+    GetRoomKeyUseCase
+} from "..";
+
+export class CreateCheckInUseCase<
+    Command extends ICreateCheckIn = ICreateCheckIn,
+    Response extends ICheckInCreatedResponse = ICheckInCreatedResponse
+> extends ValueObjectErrorHandler implements IUseCase<Command, Response>
+{
+    private readonly checkInAggregate: CheckInAggregate;
+    private readonly getGuestUseCase: GetGuestUseCase;
+    private readonly getRoomKeyUseCase: GetRoomKeyUseCase;
+
+    constructor(
+        private readonly checkInService: ICheckInDomainService,
+        private readonly checkInCreatedEventPublisher: CheckInCreatedEventPublisher,
+        private readonly guestObtainedEventPublisher: GuestObtainedEventPublisher,
+        private readonly roomKeyObtainedEventPublisher: RoomKeyObtainedEventPublisher,
+    ) {
+        super();
+        this.checkInAggregate = new CheckInAggregate({
+            checkInService,
+            checkInCreatedEventPublisher
+        })
+    }
+
+    async execute(command?: Command): Promise<Response> {
+        const data = await this.executeCommand(command);
+
+        return { success: data ? true : false, data } as unknown as Response
+    }
+
+
+    private async executeCommand(command: Command): Promise<CheckInDomainEntity | null> {
+        const ValueObject = this.createValueObject(command);
+        this.validateValueObject(ValueObject);
+        const checkIn = this.createEntityCheckInDomain(ValueObject, command);
+        return this.exectueCheckInAggregate(await checkIn)
+    }
+
+    private createValueObject(command: Command): ICheckInDomainEntity {
+        const recepsionistName = new FullNameValueObject(command.recepsionistName);
+        const startDate = new DateValueObject(command.startDate);
+
+        return {
+            recepsionistName,
+            startDate
+        }
+    }
+
+    private validateValueObject(valueObject: ICheckInDomainEntity): void {
+        const {
+            recepsionistName,
+            startDate
+        } = valueObject
+
+        if (recepsionistName instanceof FullNameValueObject && recepsionistName.hasErrors())
+            this.setErrors(recepsionistName.getErrors());
+
+        if (startDate instanceof DateValueObject && startDate.hasErrors())
+            this.setErrors(startDate.getErrors());
+
+        if (this.hasErrors() === true)
+            throw new ValueObjectException(
+                'Hay algunos errores en el comando ejecutado por CreateCheckInUseCase',
+                this.getErrors(),
+            );
+    }
+
+    private async createEntityCheckInDomain(valueObject: ICheckInDomainEntity, command: Command): Promise<CheckInDomainEntity> {
+        const {
+            recepsionistName,
+            startDate
+        } = valueObject
+
+        const responseGuest = this.getGuestUseCase.execute({ guestId: command.guestId })
+
+        const responseRoomKey = this.getRoomKeyUseCase.execute({ roomKeyId: command.roomKeyId })
+
+        return new CheckInDomainEntity({
+            recepsionistName: recepsionistName.valueOf(),
+            startDate: startDate,
+            guest: (await responseGuest).data,
+            roomKey: (await responseRoomKey).data,
+        })
+    }
+
+    private exectueCheckInAggregate(checkIn: ICheckInDomainEntity): Promise<CheckInDomainEntity | null> {
+        return this.checkInAggregate.createCheckIn(checkIn as ICreateCheckIn)
+    }
+}
+
