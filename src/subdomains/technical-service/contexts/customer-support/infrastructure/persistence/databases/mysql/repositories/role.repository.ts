b657b35687b8dc2a52df6db685a757d32cdfedfe
Edinson Acosta Gancharov <acostaedinson11@gmail.com>
2023-03-13 18:46:30 -0300

    RoleRepository DONE

--- src/subdomains/technical-service/contexts/customer-support/infrastructure/persistence/databases/mysql/repositories/role.repository.ts
+++ src/subdomains/technical-service/contexts/customer-support/infrastructure/persistence/databases/mysql/repositories/role.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { RoleMySqlEntity } from "../entities";
@@ -12,25 +12,86 @@ export class RoleRepository implements IRepository<RoleMySqlEntity>{
         private readonly repository: Repository<RoleMySqlEntity>
     ){}
 
-
-    findAll(): Promise<RoleMySqlEntity[]> {
-        throw new Error("Method not implemented.");
-    }
-    findById(id: string): Promise<RoleMySqlEntity> {
-        throw new Error("Method not implemented.");
-    }
-    create(entity: RoleMySqlEntity): Promise<RoleMySqlEntity> {
-        throw new Error("Method not implemented.");
-    }
-    update(id: string, entity: RoleMySqlEntity): Promise<RoleMySqlEntity> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string): Promise<boolean> {
-        throw new Error("Method not implemented.");
+    /**
+     *  Returns all the entities 
+     *
+     * @return {*}  {Promise<RoleMySqlEntity[]>}
+     * @memberof RoleRepository
+     */
+    async findAll(): Promise<RoleMySqlEntity[]> {
+        return await this.repository.find()
     }
 
-//TODO: implementar metodos
 
-    
+    /**
+     * Search in the DB a entity with the given ID
+     * gives an exception if not found
+     *
+     * @param {string} id
+     * @return {*}  {Promise<RoleMySqlEntity>}
+     * @memberof RoleRepository
+     */
+    async findById(roleID: string): Promise<RoleMySqlEntity> {
+        const result = await this.repository.findOneBy({ roleID, deletedAt: undefined });
+
+        if (!result) throw new BadRequestException(`Role with id: ${roleID} not found`);
+
+        return result;
+    }
+
+    /**
+     * Registers a new entity
+     *
+     * @param {RoleMySqlEntity} entity
+     * @return {*}  {Promise<RoleMySqlEntity>}
+     * @memberof RoleRepository
+     */
+    async create(entity: RoleMySqlEntity): Promise<RoleMySqlEntity> {
+        return await this.repository.save(entity);
+    }
+
+
+    /**
+     * Updates the information of the entity with the given ID
+     * returns and exception if not found
+     *
+     * @param {string} id
+     * @param {RoleMySqlEntity} entity
+     * @return {*}  {Promise<RoleMySqlEntity>}
+     * @memberof RoleRepository
+     */
+    async update(roleID: string, entity: RoleMySqlEntity): Promise<RoleMySqlEntity> {
+        let entityFound = await this.findById(roleID);
+
+        if (!entityFound) throw new BadRequestException(`Role with id: ${roleID} not found`);
+
+        entity.roleID = roleID; //ensures the ID is the same as the original
+
+        entityFound = { ...entityFound, ...entity };
+
+        this.repository.save(entityFound);
+
+        return entityFound;
+    }
+
+
+    /**
+     * Soft deletes the entity with the given ID
+     * is not found returns an exception
+     *
+     * @param {string} id
+     * @return {*}  {Promise<boolean>}
+     * @memberof RoleRepository
+     */
+    async delete(roleID: string): Promise<boolean> {
+        
+        const result = await this.findById(roleID);
+
+        if (!result) throw new BadRequestException(`Role with id: ${roleID} not found`);
+
+        result.deletedAt = Date.now();
+
+        return true;
+    }    
     
 }
