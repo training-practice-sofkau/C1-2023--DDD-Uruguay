e0012d8406746e2709438370e513708573e1690b
Cristian Tironi <tironi741@gmail.com>
2023-03-13 18:06:27 -0300

    Services Guest
    Creamos e Implementamos el service de Guest

--- src/subdomains/client-service/contexts/reserve-management/domain/services/guest.domain-service.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/services/guest.domain-service.ts
@@ -1,7 +1,7 @@
 import { GuestDomainEntity } from "../entities";
 import { IUpdateEmail, IUpdatePhone } from "../interfaces";
 
-export interface IGuestDomainService {
+export interface IGuestDomainService<T extends GuestDomainEntity = GuestDomainEntity> {
     updatePhone(data: IUpdatePhone): Promise<GuestDomainEntity>;
     updateEmail(data: IUpdateEmail): Promise<GuestDomainEntity>;
 }
