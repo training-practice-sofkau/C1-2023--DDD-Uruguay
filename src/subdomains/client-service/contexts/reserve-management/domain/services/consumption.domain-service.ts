66fabfbd08b3b19ebb4e2a4d9d311f7e93223cbb
Cristian Tironi <tironi741@gmail.com>
2023-03-13 18:00:42 -0300

    Services Consumption
    Creamos e Implementamos el service de consumption

--- src/subdomains/client-service/contexts/reserve-management/domain/services/consumption.domain-service.ts
+++ src/subdomains/client-service/contexts/reserve-management/domain/services/consumption.domain-service.ts
@@ -1,7 +1,7 @@
 import { ConsumptionDomainEntity } from "../entities";
 import { IUpdateExtra, IUpdateMiniBar } from "../interfaces";
 
-export interface IConsumptionDomainService {
+export interface IConsumptionDomainService<T extends ConsumptionDomainEntity = ConsumptionDomainEntity> {
     updateExtra(data: IUpdateExtra): Promise<ConsumptionDomainEntity>;
     updateMiniBar(data: IUpdateMiniBar): Promise<ConsumptionDomainEntity>;
 }
