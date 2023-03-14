17f7e02811d9d8e20a509648d1e417e04032295d
Franco Torres <franco.torres1705@gmail.com>
2023-03-14 12:33:54 -0300

    creando commands

--- src/subdomains/STORE/contexts/Customer-Service/infrastructure/utils/commands/order/IupdatePhoneClient.ts
+++ src/subdomains/STORE/contexts/Customer-Service/infrastructure/utils/commands/order/IupdatePhoneClient.ts
@@ -1,2 +1,8 @@
-export class IupdatePhoneClient {
+import { IsString, IsUUID } from 'class-validator';
+import { UpdatePhoneClient } from 'src/subdomains/Store/contexts/Customer-Service/domain/interfaces/commands';
+export class IupdatePhoneClient implements  UpdatePhoneClient{
+    @IsUUID()
+    ClientID: string;
+    @IsString()
+    Phone: number;
 }
