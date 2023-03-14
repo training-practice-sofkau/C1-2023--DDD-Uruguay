377314aa55f68128b458059f659cca98db4b2066
Cristian Tironi <tironi741@gmail.com>
2023-03-14 18:21:33 -0300

    Infrastructure Controllers CheckIn
    Implementamos el controllador del agregado CheckIn, con todos sus metodos y el de sus entidades

--- src/subdomains/client-service/contexts/reserve-management/infrastructure/messaging/publisher/check-in/index.ts
+++ src/subdomains/client-service/contexts/reserve-management/infrastructure/messaging/publisher/check-in/index.ts
@@ -1,5 +1,7 @@
 export * from './check-in-created.message-publisher';
 export * from './guest';
 export * from './guest-added.message-publisher';
+export * from './guest-obtained.message-publisher';
+export * from './guest-room-key.message-publisher';
 export * from './room-key';
 export * from './room-key-added.message-publisher';
