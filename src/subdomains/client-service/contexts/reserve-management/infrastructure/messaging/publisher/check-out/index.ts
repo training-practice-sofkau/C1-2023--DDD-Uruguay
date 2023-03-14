58d8de202920dd0d53f1aed881a758cf0672f495
Cristian Tironi <tironi741@gmail.com>
2023-03-14 18:40:34 -0300

    Infrastructure Controllers CheckOut
    Implementamos el controllador del agregado CheckOut, con todos sus metodos y el de sus entidades

--- src/subdomains/client-service/contexts/reserve-management/infrastructure/messaging/publisher/check-out/index.ts
+++ src/subdomains/client-service/contexts/reserve-management/infrastructure/messaging/publisher/check-out/index.ts
@@ -1,5 +1,7 @@
 export * from './check-out-created.message-publisher';
 export * from './consumption';
 export * from './consumption-added.message-publisher';
+export * from './consumption-obtained.message-publisher';
 export * from './invoice';
 export * from './invoice-added.message-publisher';
+export * from './invoice-obtained.message-publisher';
