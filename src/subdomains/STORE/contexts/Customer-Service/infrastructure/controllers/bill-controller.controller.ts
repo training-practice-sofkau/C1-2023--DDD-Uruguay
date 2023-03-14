47c3b4c8229812afdea1f604541a34232a2ee7c8
Franco Torres <franco.torres1705@gmail.com>
2023-03-14 14:27:00 -0300

    cambios en bill controller

--- src/subdomains/STORE/contexts/Customer-Service/infrastructure/controllers/bill-controller.controller.ts
+++ src/subdomains/STORE/contexts/Customer-Service/infrastructure/controllers/bill-controller.controller.ts
@@ -1,15 +1,18 @@
+import { IPaymentMethodEventPublisher } from './../messaging/publisher/Sale/Bill/Payment-method-messaging-publisher';
 /*
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Get } from '@nestjs/common';
-import { GetBillUseCase, GetClientCaseUse } from '../../application';
+import { Body, Controller, Get, Put } from '@nestjs/common';
+import { GetBillUseCase, GetClientCaseUse, UpdatePaymentUseCase, UpdateTotalUseCase } from '../../application';
 import { IupdatePaymentMethod } from '../utils/commands/sale/IupdatePaymentMethod';
 import { BillService } from '../persitence/services/SaleServices/BillService';
 import { BillMySqlService } from '../persitence/databases/mysql/services/IBill-Domain-Service';
 import { SaleService } from '../persitence/services/SaleServices/SaleService';
 import { IBillObtainedEventPublisher } from '../messaging/publisher/Sale/IBillObtainedEventPublisher';
 import { IGetBill } from '../utils/commands/sale/IGetBill';
+import { IupdateTotalcommand } from '../utils/commands/sale/IupdateTotal';
+import { ITotalModifiedEventPublisher } from '../messaging/publisher/Sale/Bill/total-modified--messaging-publisher';
 
 @Controller('Bill')
 export class BillControllerController {
@@ -18,18 +21,40 @@ export class BillControllerController {
     constructor(
         private readonly BillService: BillService,
         private readonly SaleService: SaleService,
-        private readonly IBillObtainedEventPublisher:IBillObtainedEventPublisher
+        private readonly IBillObtainedEventPublisher:IBillObtainedEventPublisher,
+        private readonly ITotalModifiedEventPublisher:ITotalModifiedEventPublisher,
+        private readonly IPaymentMethodEventPublisher:IPaymentMethodEventPublisher
+
     
     
       ) {}
 
 
     @Get()
-    getClient(@Body() command: IGetBill) {
+    getBill(@Body() command: IGetBill) {
       const useCase = new  GetBillUseCase (this.SaleService,  this.IBillObtainedEventPublisher)
       return useCase.execute(command)
       
     }
+
+    @Put('Bill-total')
+    updateBillTotal(@Body() command: IupdateTotalcommand) {
+      const useCase = new UpdateTotalUseCase(
+        this.BillService,
+        this.ITotalModifiedEventPublisher,
+      );
+      useCase.execute(command);
+    }
+  
+    @Put('Bill-Payment-Method')
+    updateBillPyment(@Body() command: IupdatePaymentMethod) {
+      const useCase = new UpdatePaymentUseCase(
+        this.BillService,
+        this.IPaymentMethodEventPublisher,
+      );
+      useCase.execute(command);
+    }
+  
 
 
 }
