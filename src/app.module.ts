8b782648df5b48427e31c1ed619178811f28c56d
Edinson Acosta Gancharov <acostaedinson11@gmail.com>
2023-03-13 22:14:55 -0300

    Connection To DB fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { join } from 'path';
+import { join } from 'node:path';
 import { TechnicalServiceModule } from './subdomains/technical-service/contexts/customer-support/infrastructure/technical-service.module';
 
 @Module({
   imports: [
     TechnicalServiceModule,
     ConfigModule.forRoot({
+      isGlobal:true,
       envFilePath: join(
         process.cwd(),
         'environments',
         `.env.${process.env.SCOPE?.trimEnd()}`,
       ),
-      isGlobal:true,
-    })
+    }),   
   ],
   controllers: [],
   providers: [],
