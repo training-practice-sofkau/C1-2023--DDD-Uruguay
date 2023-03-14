6e8a57c00c91018503e0383757f17c87c0ff5c71
Franco Torres <franco.torres1705@gmail.com>
2023-03-14 12:56:09 -0300

    agregando commands

--- src/subdomains/STORE/contexts/Customer-Service/infrastructure/utils/commands/order/IUpdateMangaStock.ts
+++ src/subdomains/STORE/contexts/Customer-Service/infrastructure/utils/commands/order/IUpdateMangaStock.ts
@@ -1,9 +1,10 @@
 import { IUpdateMangaStock } from "../../../../domain/interfaces/commands";
-import { IsUUID } from 'class-validator';
+import { IsNumber,  IsUUID } from 'class-validator';
 
 export class IUpdateMangaStockCommand implements IUpdateMangaStock {
 
     @IsUUID()
     MangaId: string;
+    @IsNumber()
     MangaStock: number;
 }
