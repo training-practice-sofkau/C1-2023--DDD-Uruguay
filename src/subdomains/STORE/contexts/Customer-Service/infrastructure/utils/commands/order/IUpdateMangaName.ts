9dc8f49bc4552994a34d35b112a9dbb0f9ed3b67
Franco Torres <franco.torres1705@gmail.com>
2023-03-14 12:45:07 -0300

    update name command

--- src/subdomains/STORE/contexts/Customer-Service/infrastructure/utils/commands/order/IUpdateMangaName.ts
+++ src/subdomains/STORE/contexts/Customer-Service/infrastructure/utils/commands/order/IUpdateMangaName.ts
@@ -1,2 +1,9 @@
-export class IUpdateMangaName {
+import { IsString, IsUUID } from "class-validator";
+import { UpdateNameManga } from "../../../../domain/interfaces/commands/Order-commands";
+
+export class IUpdateMangaName implements UpdateNameManga {
+    @IsUUID()
+    MangaId: string;
+    @IsString()
+    newName: string;
 }
--- src/subdomains/STORE/contexts/Customer-Service/infrastructure/utils/commands/order/IupdateMangaPrice.ts
+++ src/subdomains/STORE/contexts/Customer-Service/infrastructure/utils/commands/order/IupdateMangaPrice.ts
@@ -1,2 +1,10 @@
-export class IupdateMangaPrice {
+import {  IsUUID,  IsNumber } from 'class-validator';
+import { UpdatePriceManga } from './../../../../domain/interfaces/commands/Order-commands/Manga-Commands/update-price-command';
+import {  } from '../../../../../../../../libs/validations/is-number.validations';
+export class IupdateMangaPrice implements UpdatePriceManga {
+    @IsUUID()
+    MangaId: string;
+
+    @IsNumber()
+    newPrice: number;
 }
